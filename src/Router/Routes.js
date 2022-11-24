4e6e36ad702ec437b760c087ade421b13e8c7693
saifur2233 <saifurnstuiit223344@gmail.com>
2022-11-24 20:10:36 +0600

    navbar and footer added

---
+++ src/Router/Routes.js
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Login from "../Pages/Auth/Login";
+import SignUp from "../Pages/Auth/SignUp";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
+      },
+    ],
+  },
+]);
