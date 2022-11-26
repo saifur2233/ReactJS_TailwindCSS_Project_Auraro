9bd43ebcd42fa0d724546a413127fb72dde63dfa
MD SAIFUR RAHMAN <saifurnstuiit223344@gmail.com>
2022-11-26 20:55:17 +0600

    Dashboard Layout added

---
+++ src/Layout/DashboardLayout.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "../Shared/Header/NavBar";
+
+const DashboardLayout = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div className="drawer drawer-mobile">
+        <input
+          id="dashboard-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+        />
+        <div className="drawer-content">
+          <Outlet></Outlet>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+            <li>
+              <Link to="/dashboard">My Order</Link>
+            </li>
+            <li>
+              <Link to="/dashboard/users">All Users</Link>
+            </li>
+            <li>
+              <Link to="/dashboard/addproduct">Add Product</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
---
+++ src/Pages/Dashboard/AllBuyer/AllBuyer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllBuyer = () => {
+  return <div>AllBuyer</div>;
+};
+
+export default AllBuyer;
