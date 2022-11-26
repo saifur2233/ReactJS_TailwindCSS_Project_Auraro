e7179e8bc4e7d550e51141f6c453eb863dbf4b5f
MD SAIFUR RAHMAN <saifurnstuiit223344@gmail.com>
2022-11-26 21:47:28 +0600

    dashboard route added

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -27,6 +27,12 @@ const DashboardLayout = () => {
             <li>
               <Link to="/dashboard/addproduct">Add Product</Link>
             </li>
+            <li>
+              <Link to="/dashboard/allseller">All Seller</Link>
+            </li>
+            <li>
+              <Link to="/dashboard/allbuyer">All Buyer</Link>
+            </li>
           </ul>
         </div>
       </div>
--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -1,8 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../Layout/DashboardLayout";
 import Main from "../Layout/Main";
 import Login from "../Pages/Auth/Login";
 import SignUp from "../Pages/Auth/SignUp";
 import Blogs from "../Pages/Blogs/Blogs";
+import AllBuyer from "../Pages/Dashboard/AllBuyer/AllBuyer";
+import AllSeller from "../Pages/Dashboard/AllSeller/AllSeller";
+import AddProduct from "../Pages/Dashboard/AllUsers/AddProduct/AddProduct";
+import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
+import MyOrders from "../Pages/Dashboard/MyOrders/MyOrders";
 import NotFound from "../Pages/Error/NotFound";
 import Home from "../Pages/Home/Home";
 
@@ -30,6 +36,32 @@ export const router = createBrowserRouter([
       {
         path: "*",
         element: <NotFound></NotFound>,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout></DashboardLayout>,
+    children: [
+      {
+        path: "/dashboard",
+        element: <MyOrders></MyOrders>,
+      },
+      {
+        path: "/dashboard/users",
+        element: <AllUsers></AllUsers>,
+      },
+      {
+        path: "/dashboard/allseller",
+        element: <AllSeller></AllSeller>,
+      },
+      {
+        path: "/dashboard/allbuyer",
+        element: <AllBuyer></AllBuyer>,
+      },
+      {
+        path: "/dashboard/addproduct",
+        element: <AddProduct></AddProduct>,
       },
     ],
   },
